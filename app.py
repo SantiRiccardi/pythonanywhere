9f8b41f40263af55f5923975661ba905d03c5e21
SantiRiccardi <santi.riccardi@gmail.com>
2023-03-14 15:55:11 -0300

    incorpore bla

--- app.py
+++ app.py
@@ -8,27 +8,27 @@ app.config['DEBUG'] = True
 @app.route('/', methods=['GET'])
 def index():
 
-    return 'mi primera Web cutre....'
+    return 'mi primera Web cutre....soy Santi Riccardi '
 
 @app.route('/v2/', methods=['GET'])
 def otra():
 
     return 'otra pag'
 
-#@app.route('/api/v1/predict', methods=['GET'])
-#def predict():
+@app.route('/api/v1/predict', methods=['GET'])
+def predict():
 
-    #model = pickle.load(open('ad_model.pkl','rb'))
-    #tv = float(request.args.get('tv', None))
-    #radio = float(request.args.get('radio', None))
-    #newspaper = float(request.args.get('newspaper', None))
+    model = pickle.load(open('/home/SantiRiccardi/pythonanywhere/finished_model.pkl','rb'))
+    tv = float(request.args.get('tv', None))
+    radio = float(request.args.get('radio', None))
+    newspaper = float(request.args.get('newspaper', None))
 
-    #if tv is None or radio is None or newspaper is None:
-        #return "Args empty, the data are not enough to predict"
-    #else:
-        #prediction = model.predict([[tv,radio,newspaper]])
+    if tv is None or radio is None or newspaper is None:
+        return "Args empty, the data are not enough to predict"
+    else:
+        prediction = model.predict([[tv,radio,newspaper]])
 
-    #return jsonify({'predictions': prediction[0]})
+    return jsonify({'predictions': prediction[0]})
 
 
 
